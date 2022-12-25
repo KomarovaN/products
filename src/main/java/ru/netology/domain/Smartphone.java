d8f526cb22c238de97f5c229be6d41d6cd7fbfa4
Nadezhda Komarova <grukhinan@gmail.com>
2022-12-25 21:22:12 +0300

    Added branch rich

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,10 +1,23 @@
 package ru.netology.domain;
 
-public class Smartphone extends Product{
+public class Smartphone extends Product {
     String maker;
 
     public Smartphone(int id, String name, int cost, String maker) {
         super(id, name, cost);
         this.maker = maker;
     }
+
+    public boolean matches(String search) {
+        if (super.matches(search)) { // вызов метода matches в версии описанной в Product
+            return true;
+        }
+        return getMaker().contains(search);
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
 }
+
